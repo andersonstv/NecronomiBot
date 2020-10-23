4a5864cdbf61f8084db462ca2e518ef6980feee3
anderson santos <anderson.santos.silva@ccc.ufcg.edu.br>
2020-10-23 19:47:07 -0300

    updated Guild join message

--- src/main/java/io/github/andersonstv/MessageListener.java
+++ src/main/java/io/github/andersonstv/MessageListener.java
@@ -45,9 +45,14 @@ public class MessageListener extends ListenerAdapter {
     @Override
     public void onGuildJoin(@NotNull GuildJoinEvent event) {
         MessageChannel channel = event.getGuild().getSystemChannel();
+        String sep = System.lineSeparator();
+        String introMessage = "*That is not dead which can eternal lie." + sep +
+                "And with strange aeons even death may die.*" + sep +
+                "Hello, I'm NecronomiBot, a bot made to help running Call of Cthulhu (and other RPGs) " +
+                "on Discord. You can check out the source code at: " +
+                "https://github.com/andersonstv/NecronomiBot";
         if (channel != null){
-            channel.sendMessage("That is not dead which can eternal lie." + System.lineSeparator() +
-                    "And with strange aeons even death may die.").queue();
+            channel.sendMessage(introMessage).queue();
         }
     }
 
